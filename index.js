ac40ee8d7445d3effbfbb352be6b44d307df7a96
Sarah Riehl <sls.riehl@gmail.com>
2016-10-06 18:30:13 -0500

    log.txt fixed

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function myTweets() {
 			for(var t = 0; t < tweets.length; t++) {
 				output = ('\n' + '@' + params.screen_name + ' said ' + tweets[t].text + ' at ' + tweets[t].created_at +'\n');
 				console.log(output);
+				append();
 			}
 		} else {
 			console.log('twitter error');
@@ -40,6 +41,7 @@ function spotifyThisSong() {
 				}
 				output = ('\nSong Info \n\nArtist: ' + artistConcat + '\n\nSong Title: ' + thisSong.name + '\n\nOriginal Album: ' + thisSong.album.name + '\n\nPreview: ' + thisSong.preview_url + '\n');
 				console.log(output);
+				append();
 			
 			} else {
 				console.log('spotify error or there is no song matching that title.');
@@ -63,6 +65,7 @@ function movieThis() {
 		if(!error && response.statusCode == 200) {
 			output = ('\nMovie Info \n\nTitle: ' + JSON.parse(body)['Title'] + '\n\nRelease Date: ' + JSON.parse(body)['Released'] + '\n\nIMDB Rating: ' + JSON.parse(body)['imdbRating'] + '\n\nProduction Country: ' + JSON.parse(body)['Country'] + '\n\nLanguage: ' + JSON.parse(body)['Language'] + '\n\nSynopsis: ' + JSON.parse(body)['Plot'] + '\n\nActors: ' + JSON.parse(body)['Actors'] + '\n\nRotten Tomatoes Rating: ' + JSON.parse(body)['tomatoRating'] + '\n\nLearn more at Rotten Tomatoes: ' + JSON.parse(body)['tomatoURL'] + '\n');
 			console.log(output); 
+			append();
 		} else {
 			console.log('omdb error or nothing matches your search.');
 		}
@@ -72,8 +75,9 @@ function doWhatItSays() {
 	var pickRandom = Math.floor(Math.random() * random.commands.length);
 	command = random.commands[pickRandom].comm;
 	argument = random.commands[pickRandom].argu;
-	output = ("\nyou've selected " + random.commands[pickRandom].comm + " to learn more about " + random.commands[pickRandom].argu + '.\n');
+	output = ("\nyou've selected the random command " + random.commands[pickRandom].comm + " to learn more about " + random.commands[pickRandom].argu + '.\n');
 	choose();
+	append();
 }
 
 function choose() {
@@ -93,6 +97,10 @@ function choose() {
 		default:
 			console.log("I don't know how to do that.  Please try again!");
 	}
+
+}
+
+function append() {
 	fs.appendFile('log.txt', output, function callback(error) {
 		if(!error) {
 			console.log('this data has been added to your log.');
@@ -100,7 +108,6 @@ function choose() {
 			console.log("sorry, the data didn't write to file.");
 		}
 	});
-
 }
 
 choose();
