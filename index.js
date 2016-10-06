da7532be91e292b838052ba57e575aeab6a8b76a
Sarah Riehl <sls.riehl@gmail.com>
2016-10-06 16:58:40 -0500

    still a log bug

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ var spotify = require('spotify');
 var fs = require('fs');
 var keys = require('./keys.js');
 var querystring = require('querystring');
-var randomText = require('./random.js');
+var random = require('./random.js');
+
+var command = process.argv[2];
+var argument = process.argv[3];
+var output;
 
 function myTweets() {
 	var params = {
@@ -13,7 +17,8 @@ function myTweets() {
 	twitterKeys.get('statuses/user_timeline', params, function(error, tweets, response) {
 		if(!error) {
 			for(var t = 0; t < tweets.length; t++) {
-				console.log('\n' + '@' + params.screen_name + ' said ' + tweets[t].text + ' at ' + tweets[t].created_at +'\n');
+				output = ('\n' + '@' + params.screen_name + ' said ' + tweets[t].text + ' at ' + tweets[t].created_at +'\n');
+				console.log(output);
 			}
 		} else {
 			console.log('twitter error');
@@ -22,11 +27,10 @@ function myTweets() {
 }
 
 function spotifyThisSong() {
-	var spotQuery = process.argv[3];
-	if(!process.argv[3]) {
-		spotQuery = 'The Sign Ace of Base';
+	if(argument == null) {
+		argument = 'The Sign Ace of Base';
 	}
-	spotify.search({type: 'track', query: spotQuery}, function(error, data) {
+	spotify.search({type: 'track', query: argument}, function(error, data) {
 		var thisSong;
 			if(!error && (data.tracks.items.length >= 1)) {
 				thisSong = data.tracks.items[0];
@@ -34,7 +38,8 @@ function spotifyThisSong() {
 				for(var a = 1; a < thisSong.artists.length; a++) {
 					artistConcat += ', ' + thisSong.artists[a].name;
 				}
-				console.log('\nSong Info \n\nArtist: ' + artistConcat + '\n\nSong Title: ' + thisSong.name + '\n\nOriginal Album: ' + thisSong.album.name + '\n\nPreview: ' + thisSong.preview_url + '\n');
+				output = ('\nSong Info \n\nArtist: ' + artistConcat + '\n\nSong Title: ' + thisSong.name + '\n\nOriginal Album: ' + thisSong.album.name + '\n\nPreview: ' + thisSong.preview_url + '\n');
+				console.log(output);
 			
 			} else {
 				console.log('spotify error or there is no song matching that title.');
@@ -43,42 +48,59 @@ function spotifyThisSong() {
 }
 
 function movieThis() {
+	if(argument == null) {
+		argument = 'Mr. Nobody';
+	}
 	var queryProps = {
-		t: process.argv[3],
+		t: argument,
 		plot: 'full',
 		r: 'json',
 		tomatoes: true
-	}
-	if(!process.argv[3]) {
-		queryProps.t = 'Mr. Nobody';
 	}
 	var queryUrl = 'http://www.omdbapi.com/?' + querystring.stringify(queryProps);
 
 	request(queryUrl, function(error, response, body) {
 		if(!error && response.statusCode == 200) {
-			console.log('\nMovie Info \n\nTitle: ' + JSON.parse(body)['Title'] + '\n\nRelease Date: ' + JSON.parse(body)['Released'] + '\n\nIMDB Rating: ' + JSON.parse(body)['imdbRating'] + '\n\nProduction Country: ' + JSON.parse(body)['Country'] + '\n\nLanguage: ' + JSON.parse(body)['Language'] + '\n\nSynopsis: ' + JSON.parse(body)['Plot'] + '\n\nActors: ' + JSON.parse(body)['Actors'] + '\n\nRotten Tomatoes Rating: ' + JSON.parse(body)['tomatoRating'] + '\n\nLearn more at Rotten Tomatoes: ' + JSON.parse(body)['tomatoURL'] + '\n'); 
+			output = ('\nMovie Info \n\nTitle: ' + JSON.parse(body)['Title'] + '\n\nRelease Date: ' + JSON.parse(body)['Released'] + '\n\nIMDB Rating: ' + JSON.parse(body)['imdbRating'] + '\n\nProduction Country: ' + JSON.parse(body)['Country'] + '\n\nLanguage: ' + JSON.parse(body)['Language'] + '\n\nSynopsis: ' + JSON.parse(body)['Plot'] + '\n\nActors: ' + JSON.parse(body)['Actors'] + '\n\nRotten Tomatoes Rating: ' + JSON.parse(body)['tomatoRating'] + '\n\nLearn more at Rotten Tomatoes: ' + JSON.parse(body)['tomatoURL'] + '\n');
+			console.log(output); 
 		} else {
 			console.log('omdb error or nothing matches your search.');
 		}
 	});
 }
 function doWhatItSays() {
+	var pickRandom = Math.floor(Math.random() * random.commands.length);
+	command = random.commands[pickRandom].comm;
+	argument = random.commands[pickRandom].argu;
+	output = ("\nyou've selected " + random.commands[pickRandom].comm + " to learn more about " + random.commands[pickRandom].argu + '.\n');
+	choose();
+}
+
+function choose() {
+	switch(command) {
+		case 'my-tweets':
+			myTweets();
+			break;
+		case 'spotify-this-song':
+			spotifyThisSong();
+			break;
+		case 'movie-this':
+			movieThis();
+			break;
+		case 'do-what-it-says':
+			doWhatItSays();
+			break;
+		default:
+			console.log("I don't know how to do that.  Please try again!");
+	}
+	fs.appendFile('log.txt', output, function callback(error) {
+		if(!error) {
+			console.log('this data has been added to your log.');
+		} else {
+			console.log("sorry, the data didn't write to file.");
+		}
+	});
 
 }
 
-switch(process.argv[2]) {
-	case 'my-tweets':
-		myTweets();
-		break;
-	case 'spotify-this-song':
-		spotifyThisSong();
-		break;
-	case 'movie-this':
-		movieThis();
-		break;
-	case 'do-what-it-says':
-		doWhatItSays();
-		break;
-	default:
-		console.log("I don't know how to do that.  Please try again!");
-}
+choose();
---
+++ random.js
@@ -0,0 +1,43 @@
+exports.commands = [
+	{
+		comm: 'spotify-this-song',
+		argu: 'Tears Dry on Their Own'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: 'Eye in the Sky'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: 'Heart-Shaped Box'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Blade Runner'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Sliding Doors'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'A League of Their Own'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: 'American Woman'
+	},
+	{
+		comm: 'spotify-this-song',
+		argu: "Baba O'Riley"
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Stick It'
+	},
+	{
+		comm: 'movie-this',
+		argu: 'Dumbo'
+	}
+];
+
